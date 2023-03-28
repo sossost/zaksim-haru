105419cf89625b90bc5c8e24dc18e4e10862a644
sossost <110542210+sossost@users.noreply.github.com>
2023-03-28 19:13:16 +0900

    라디오버튼 선택 기능 리팩토링

--- src/Modal.js
+++ src/Modal.js
@@ -46,43 +46,56 @@ const sunFitureBtn = document.querySelector("#sun");
 const pillFitureBtn = document.querySelector("#pill");
 const fitureInputEl = document.querySelector(".jaksim_today_modal_color_check");
 
-const waterClickHandler = () => {
-  waterFitureBtn.classList.add("water_clicked");
-  sunFitureBtn.classList.remove("sun_clicked");
-  pillFitureBtn.classList.remove("pill_clicked");
-  fitureInputEl.setAttribute("value", "water");
-};
-
-const sunClickHandler = () => {
-  waterFitureBtn.classList.remove("water_clicked");
-  sunFitureBtn.classList.add("sun_clicked");
-  pillFitureBtn.classList.remove("pill_clicked");
-  fitureInputEl.setAttribute("value", "sun");
-};
-
-const pillClickHandler = () => {
+const fitureClickHandler = (fiture) => {
   waterFitureBtn.classList.remove("water_clicked");
   sunFitureBtn.classList.remove("sun_clicked");
-  pillFitureBtn.classList.add("pill_clicked");
-  fitureInputEl.setAttribute("value", "pill");
-};
-// 선택한 특성의 value는 fitureInputEl의 value에 저장
+  pillFitureBtn.classList.remove("pill_clicked");
+  // 각 버튼들 clicked css 초기화
 
-waterFitureBtn.addEventListener("click", waterClickHandler);
-sunFitureBtn.addEventListener("click", sunClickHandler);
-pillFitureBtn.addEventListener("click", pillClickHandler);
+  if (fiture === "water") {
+    waterFitureBtn.classList.add("water_clicked");
+  } else if (fiture === "sun") {
+    sunFitureBtn.classList.add("sun_clicked");
+  } else if (fiture === "pill") {
+    pillFitureBtn.classList.add("pill_clicked");
+  }
+  // 매개변수로 받은 특징 clicked css 활성화
+
+  fitureInputEl.setAttribute("value", fiture);
+  // 클릭한 특징 부모 div value에 저장
+};
+
+waterFitureBtn.addEventListener("click", () => {
+  fitureClickHandler("water");
+});
+sunFitureBtn.addEventListener("click", () => {
+  fitureClickHandler("sun");
+});
+pillFitureBtn.addEventListener("click", () => {
+  fitureClickHandler("pill");
+});
 ///////////////////////////////////////////////////////
 
 /* 모달창 제출 버튼 */
 const modalSubmitBtn = document.querySelector(".jaksim_today_modal_submit_btn");
 const modalSubmitHandler = (e) => {
   e.preventDefault();
+
   const zaksimInputValue = document.querySelector(
     ".jaksim_today_modal_input"
   ).value;
+  // 작심 input으로 받은 value 값
+
   const zaksimFitureValue = fitureInputEl.getAttribute("value");
+  // 체크한 작심 특징 value 값
 
   console.log(zaksimInputValue, zaksimFitureValue);
+
+  if (zaksimInputValue === "") {
+    alert("작심을 입력해주세요.");
+    return;
+  }
+  // 작심 input 유효성 검사 (빈칸)
 };
 
 modalSubmitBtn.addEventListener("click", modalSubmitHandler);
