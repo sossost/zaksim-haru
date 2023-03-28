1e79849a5c8e61b56a2ebd625d6a4f7d9c049917
sossost <110542210+sossost@users.noreply.github.com>
2023-03-28 19:19:52 +0900

    라디오버튼 선택 기능 리팩토링

--- src/Modal.js
+++ src/Modal.js
@@ -47,17 +47,17 @@ const pillFitureBtn = document.querySelector("#pill");
 const fitureInputEl = document.querySelector(".jaksim_today_modal_color_check");
 
 const fitureClickHandler = (fiture) => {
-  waterFitureBtn.classList.remove("water_clicked");
-  sunFitureBtn.classList.remove("sun_clicked");
-  pillFitureBtn.classList.remove("pill_clicked");
+  waterFitureBtn.classList.remove("clicked");
+  sunFitureBtn.classList.remove("clicked");
+  pillFitureBtn.classList.remove("clicked");
   // 각 버튼들 clicked css 초기화
 
   if (fiture === "water") {
-    waterFitureBtn.classList.add("water_clicked");
+    waterFitureBtn.classList.add("clicked");
   } else if (fiture === "sun") {
-    sunFitureBtn.classList.add("sun_clicked");
+    sunFitureBtn.classList.add("clicked");
   } else if (fiture === "pill") {
-    pillFitureBtn.classList.add("pill_clicked");
+    pillFitureBtn.classList.add("clicked");
   }
   // 매개변수로 받은 특징 clicked css 활성화
 
@@ -95,7 +95,7 @@ const modalSubmitHandler = (e) => {
     alert("작심을 입력해주세요.");
     return;
   }
-  // 작심 input 유효성 검사 (빈칸)
+  // 작심 input 유효성 검사 (빈칸 제출)
 };
 
 modalSubmitBtn.addEventListener("click", modalSubmitHandler);
