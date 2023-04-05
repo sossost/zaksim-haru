721040e716b0e17e39d8e65189ba54cce60f2376
sossost <110542210+sossost@users.noreply.github.com>
2023-04-05 19:39:47 +0900

    오늘의 일기 기능 구현

--- src/EvaluationToday.js
+++ src/EvaluationToday.js
@@ -1,3 +1,5 @@
+const diaryParagraph = document.querySelector(".diary_paragraph");
+
 function emojiClickHandler(e) {
   if (e.target.className === "emoji") {
     const selectedEmoji = document.querySelector(".selected_emoji");
@@ -7,13 +9,28 @@ function emojiClickHandler(e) {
 }
 
 function diaryParagrapthClickHandler(e) {
-  if (e.target.className === "diray_paragraph") {
-    const content = e.target.innerText;
-    document.querySelector(
-      ".diary_content"
-    ).innerHTML = `<textarea class="diary_textarea" >${content}</textarea>`;
-  }
+  // if (e.target.className === "diray_paragraph") {
+  const content = e.target.innerText;
+  document.querySelector(
+    ".diary_content"
+  ).innerHTML = `<textarea class="diary_textarea" >${content}</textarea>`;
+  // }
+  const diaryTextarea = document.querySelector(".diary_textarea");
+  diaryTextarea.focus();
+
+  diaryTextarea.addEventListener("focusout", diaryTextareaFocusoutHandler);
+}
+
+function diaryTextareaFocusoutHandler(e) {
+  const content = e.target.value;
+  document.querySelector(
+    ".diary_content"
+  ).innerHTML = `<p class ="diary_paragraph">${content}</p>`;
+
+  const diaryParagraph = document.querySelector(".diary_paragraph");
+  diaryParagraph.addEventListener("click", diaryParagrapthClickHandler);
 }
 
 document.addEventListener("click", emojiClickHandler);
-document.addEventListener("click", diaryParagrapthClickHandler);
+// document.addEventListener("click", diaryParagrapthClickHandler);
+diaryParagraph.addEventListener("click", diaryParagrapthClickHandler);
